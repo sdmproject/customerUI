aaf0ea0951cd92db22f9957f3e9791d635a71720
howardon951 <wasd5123@gmail.com>
2022-05-04 11:52:45 +0800

    homepage refine

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -35,16 +35,23 @@ const Home = ({ resturants, setResturantID }) => {
           alt="Background"
         />
       </div>
-      <div>{hintText}</div>
-      {resturants.length > 0 ? (
-        <div key={0}>
-          <Restaurant
-            restaurant={resturants[0]}
-            setResturantID={setResturantID}
-            setHintText={setHintText}
-          />
+      {/* <div>{hintText}</div> */}
+      <div class="ts-grid">
+        <div class="column is-1-wide"></div>
+        <div class="column is-14-wide">
+          {resturants.length > 0 ? (
+            <div key={0}>
+              <Restaurant
+                restaurant={resturants[0]}
+                setResturantID={setResturantID}
+                setHintText={setHintText}
+              />
+            </div>
+          ) : null}
         </div>
-      ) : null}
+        <div class="column is-1-wide"></div>
+
+      </div>
       {/* <div key={0}>
         <Restaurant
           restaurant={resturants[0]}
@@ -71,6 +78,24 @@ const Home = ({ resturants, setResturantID }) => {
         }}
         size="large"
       />
+      {/* <div class="ts-space is-small"></div> */}
+      <div class="ts-grid">
+        <div class="column is-2-wide"></div>
+        <div class="column is-12-wide">
+          <div class="ts-center">
+            <div class="ts-box">
+              <div class="ts-content is-padded">
+                <div class="ts-quote is-heading ">
+                  高品質肉的沃土，消費者的餐桌之家。
+
+                  <div class="cite">meat place開業於二零一二年夏天。本店主要經營中式特色川菜。我們的川菜款式新穎，制材豐富，又不失傳統味道，開業至今一直受到廣大賓客的喜愛及好評。除此以外，我們也提供各式酒水配搭。餐廳環境優美衛生，更有充足停車位供應。我們將準備好美味的食物和優質的服務等待您的光臨！</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column is-2-wide"></div>
+      </div>
     </>
   );
 };
